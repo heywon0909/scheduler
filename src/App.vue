3e223f416832396324a2a163f0a8e9835ad2e816
heywon0909 <hw7319@naver.com>
2022-08-22 20:40:45 +0900

    scheduler 앱 초기작업중..

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex h-screen container mx-auto relative">
+    <div class="w-20  xl:w-1/4 pt-5 flex flex-col justify-between border-r border-gray-100 xl:ml-10">
+      <div class="flex flex-col items-center xl:items-start">
+        <div class="flex items-center xl:items-start mb-3">
+          <i class="fas fa-seedling text-3xl text-primary"></i>
+          <p class="text-3xl hidden xl:inline-block font-medium hidden text-primary">study</p>
+       </div>
+       <div class="flex flex-col items-start">
+       <a class="hover:text-primary hover:bg-blue-50 px-4 py-2 rounded-full cursor-pointer">
+       <i class="fas fa-calendar-alt text-2xl"></i>
+       <span class="xl:inline-block hidden text-xl ml-3">달력보기</span>
+       </a>
+      </div>
+      </div>
+    </div>
+    <div class="flex-1 flex h-screen"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
